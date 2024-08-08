00a813356c2dda416413cda235c12d48057311d1
keviocastro <keviocastro@gmail.com>
2024-08-07 23:50:47 -0300

    openalpr

--- src/placa.py
+++ src/placa.py
@@ -1,6 +1,12 @@
+import sys
 from openalpr import Alpr
 
 alpr = Alpr("br", "/srv/openalpr/config/openalpr.conf.defaults", "/srv/openalpr/config/runtime_data")
+if not alpr.is_loaded():
+    print("Error loading OpenALPR")
+    sys.exit(1)
+else:
+    print("Using OpenALPR version %s" % alpr.get_version())
 
 def get_plate_number(image_path):
     results = alpr.recognize_file(image_path)
@@ -10,12 +16,6 @@ def get_plate_number(image_path):
 
     for plate in results['results']:
         return plate['plate']
-
-def test_model_loaded():
-    if not alpr.is_loaded():
-        print("Error loading OpenALPR")
-    else:
-        print("Using OpenALPR version %s" % alpr.get_version())
 
 def unload_model():
     alpr.unload()
